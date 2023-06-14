615e16ffc6b6ed4bd63f593d305b37868134de08
DiegoVargasM <diegovargasmiranda@gmail.com>
2023-06-14 11:12:04 -0500

    Add json and urlencoding

--- server.js
+++ server.js
@@ -7,6 +7,9 @@ const productsRoutes = require("./routes/products.routes");
 
 const app = express();
 
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
 //middleware para loggear los request
 app.use((req, res, next) => {
   console.log(req.path, req.method);

5712096fff0ba40785a8b1e49020dbe471afd728
DiegoVargasM <diegovargasmiranda@gmail.com>
2023-06-14 12:58:33 -0500

    Create product controllers

---
+++ controllers/product.controller.js
@@ -0,0 +1,95 @@
+const Product = require("../models/product.model");
+//para verificar si el id es valido
+const mongoose = require("mongoose");
+
+//get all products
+const getAllProducts = async (req, res) => {
+  const products = await Product.find().sort({ createdAt: -1 });
+
+  res.status(200).json(products);
+};
+
+//get one product
+const getProduct = async (req, res) => {
+  const { id } = req.params;
+  //verificar si el id es valido
+  if (!mongoose.Types.ObjectId.isValid(id)) {
+    return res.status(404).json({ error: "Product not found" });
+  }
+
+  const product = await Product.findById(id);
+
+  //si no existe el producto devolver un error
+  if (!product) {
+    return res.status(404).json({ error: "Product not found" });
+  }
+
+  //si existe devolver el producto
+  res.status(200).json(product);
+};
+
+//create one product
+const createProduct = async (req, res) => {
+  const { product_name, amount, category, aditional_info } = req.body;
+
+  try {
+    const product = await Product.create({
+      product_name,
+      amount,
+      category,
+      aditional_info,
+    });
+    res.status(200).json({ product });
+  } catch (error) {
+    res.status(400).json({ error: error.message });
+  }
+};
+
+//delete one product
+const deleteProduct = async (req, res) => {
+  const { id } = req.params;
+  //verificar si el id es valido
+  if (!mongoose.Types.ObjectId.isValid(id)) {
+    return res.status(404).json({ error: "Product not found" });
+  }
+
+  const product = await Product.findOneAndDelete({ _id: id });
+
+  //si no existe el producto devolver un error
+  if (!product) {
+    return res.status(400).json({ error: "Product not found" });
+  }
+  res.status(200).json(product);
+};
+
+//update one product
+const updateProduct = async (req, res) => {
+  const { id } = req.params;
+
+  //verificar si el id es valido
+  if (!mongoose.Types.ObjectId.isValid(id)) {
+    return res.status(404).json({ error: "Product not found" });
+  }
+
+  const product = await Product.findOneAndUpdate(
+    { _id: id },
+    {
+      ...req.body,
+    }
+  );
+
+  //si no existe el producto devolver un error
+  if (!product) {
+    return res.status(400).json({ error: "Product not found" });
+  }
+
+  res.status(200).json(product);
+};
+
+module.exports = {
+  createProduct,
+  getAllProducts,
+  getProduct,
+  deleteProduct,
+  updateProduct,
+};
--- routes/products.routes.js
+++ routes/products.routes.js
@@ -1,30 +1,27 @@
 const express = require("express");
+const {
+  createProduct,
+  getAllProducts,
+  getProduct,
+  deleteProduct,
+  updateProduct,
+} = require("../controllers/product.controller");
 
 const router = express.Router();
 
 //get all products
-router.get("/", (req, res) => {
-  res.json({ mssg: "get all products" });
-});
+router.get("/", getAllProducts);
 
 //get one product
-router.get("/:id", (req, res) => {
-  res.json({ mssg: "get one product" });
-});
+router.get("/:id", getProduct);
 
 //post one product
-router.post("/", (req, res) => {
-  res.json({ mssg: "post one product" });
-});
+router.post("/", createProduct);
 
 //delete one product
-router.delete("/:id", (req, res) => {
-  res.json({ mssg: "delete one product" });
-});
+router.delete("/:id", deleteProduct);
 
 //update one product
-router.put("/:id", (req, res) => {
-  res.json({ mssg: "update one product" });
-});
+router.put("/:id", updateProduct);
 
 module.exports = router;

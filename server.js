1a8b3fb7fe5c62948d8f393d8ab77beb877c6c52
DiegoVargasM <diegovargasmiranda@gmail.com>
2023-06-19 16:29:11 -0500

    Add cors

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ require("dotenv").config();
 
 const express = require("express");
 const mongoose = require("mongoose");
-
+const cors = require("cors");
 //importar rutas
 const productsRoutes = require("./routes/products.routes");
 const usersRoutes = require("./routes/users.routes");
@@ -11,6 +11,7 @@ const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(cors());
 
 //middleware para loggear los request
 app.use((req, res, next) => {
